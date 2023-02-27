4d3a825d38b03c424a678406823cee9edbfd47e1
ibtesam5d <ibtesam.hossain@gmail.com>
2023-02-27 12:51:33 -0700

    component/header added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>home</main>
+      <main>
+        <Header />
+      </main>
     </>
   );
 }
