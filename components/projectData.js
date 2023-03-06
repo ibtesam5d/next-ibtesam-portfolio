1e36d5cea3a9f376b2f02765ab6f5a664fc2c1b4
ibtesam5d <ibtesam.hossain@gmail.com>
2023-03-06 09:56:34 -0700

    Live Url change and CSS adjusted for about section

--- components/projectData.js
+++ components/projectData.js
@@ -4,7 +4,7 @@ export const data = [
     title: "Snap it! NextJs Full-Stack E-commerce Shop",
     img: "/images/snap.png",
     gitUrl: "https://github.com/ibtesam5d/NextJs-E-Commerce-Project---Snap-it-",
-    liveUrl: "https://snap-it.vercel.app/",
-    desc: "This full stack project showcases my strong understanding on creating dynamic front-end components using back-end data. I used Tailwind CSS for responsive design, MongoDB and Mongoose for database connection and Redux/Redux-Toolkit for state management. Also, I integrated paypal payment method using React-Paypal-Js. React-Icons for all the icons, Axios for data fetching and JWT for client-side authentication. This shows my ability to work on a real world project and back-end knowledge to either communicate with back-end developers or be a full-stack developer. Username: admin, password:1234",
+    liveUrl: "https://snap-jt1vcwk1p-ibtesam5d.vercel.app/",
+    desc: "This full stack project showcases my strong understanding on creating dynamic front-end components using back-end data. I used Tailwind CSS for styling, MongoDB and Mongoose for database connection and Redux/Redux-Toolkit for state management. Also, I integrated paypal payment method using React-Paypal-Js. React-Icons for all the icons, Axios for data fetching and JWT for client-side authentication. This shows my ability to work on a real world project and back-end knowledge to either communicate with back-end developers or be a full-stack developer. Url:/admin name: admin, password:1234",
   },
 ];
