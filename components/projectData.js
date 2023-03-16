59becfc797573024e459e312bc9f79d4053674aa
ibtesam5d <ibtesam.hossain@gmail.com>
2023-03-16 11:30:39 -0600

    project info change

--- components/projectData.js
+++ components/projectData.js
@@ -5,7 +5,7 @@ export const data = [
     img: "/images/snap.png",
     gitUrl: "https://github.com/ibtesam5d/NextJs-E-Commerce-Project---Snap-it-",
     liveUrl: "https://snap-it.vercel.app/",
-    desc: "This full stack project showcases my strong understanding on creating dynamic front-end components using back-end data. I used Tailwind CSS for styling, MongoDB and Mongoose for database connection and Redux/Redux-Toolkit for state management. Also, I integrated paypal payment method using React-Paypal-Js. React-Icons for all the icons, Axios for data fetching and JWT for client-side authentication. This shows my ability to work on a real world project and back-end knowledge to either communicate with back-end developers or be a full-stack developer. admin name: admin, password:1234",
+    desc: "This full stack project showcases my strong understanding on creating dynamic front-end components using back-end data. I used Tailwind CSS for styling, MongoDB and Mongoose for database connection and Redux/Redux-Toolkit for state management. Also, I integrated paypal payment method using React-Paypal-Js. REST Api end-points for CRUD operation, Axios for data fetching and JWT for client-side authentication. For admin panel, name: admin, password:1234",
   },
   {
     id: 1,
@@ -13,6 +13,6 @@ export const data = [
     img: "/images/td.png",
     gitUrl: "https://github.com/ibtesam5d/webpack-td",
     liveUrl: "https://mdibtesamhossain.com/td-webpack/",
-    desc: "This is a frontend project of an online store developed using webpack. Aim of this project is to emulate an online store listing their products with a product filter feature. Filter options include: filter by categories, by price and by typing in the search box. I used JavaScript Map and Filter method along with event listeners to manipulate the DOM. Project is styled with SASS and bundled with webpack. Please checkout github for dev dependencies.",
+    desc: "This is a frontend project of an online store developed using webpack. Aim of this project is to emulate an online store listing their products with a product filter feature. Filter options include: filter by categories, by price and by typing in the search box. I used JavaScript Map and Filter method along with event listeners to manipulate the DOM. Project is styled with SASS and bundled with webpack build tools. Please checkout github for dev dependencies.",
   },
 ];
