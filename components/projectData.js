735c02eb5f486fb7b7e4c8dc8892f6db02062e72
ibtesam5d <ibtesam.hossain@gmail.com>
2023-03-26 16:39:13 -0600

    new project add, resume update

--- components/projectData.js
+++ components/projectData.js
@@ -9,10 +9,18 @@ export const data = [
   },
   {
     id: 1,
-    title: "Timeless Design - Webpack Project",
-    img: "/images/td.png",
-    gitUrl: "https://github.com/ibtesam5d/webpack-td",
-    liveUrl: "https://mdibtesamhossain.com/td-webpack/",
-    desc: "This is a frontend project of an online store developed using webpack. Aim of this project is to emulate an online store listing their products with a product filter feature. Filter options include: filter by categories, by price and by typing in the search box. I used JavaScript Map and Filter method along with event listeners to manipulate the DOM. Project is styled with SASS and bundled with webpack build tools. Please checkout github for dev dependencies.",
+    title: "Story - MERN Full-Stack Project",
+    img: "/images/story.png",
+    gitUrl: "https://github.com/ibtesam5d/MERN-Story",
+    liveUrl: "https://story-fe.onrender.com/",
+    desc: "This is a MERN stack application for authors and readers. Authors can post books and readers can purchase and post reviews. Tech used: SASS, React Router, Stripe, JWT, Bcrypt, React Query, REST Api using Express and MongoDB. Sample user: user1, pwd:user1. Backend source code here:github.com/ibtesam5d/MERN-Story-Node-Api",
   },
+  // {
+  //   id: 2,
+  //   title: "Timeless Design - Webpack Project",
+  //   img: "/images/td.png",
+  //   gitUrl: "https://github.com/ibtesam5d/webpack-td",
+  //   liveUrl: "https://mdibtesamhossain.com/td-webpack/",
+  //   desc: "This is a frontend project of an online store developed using webpack. Aim of this project is to emulate an online store listing their products with a product filter feature. Filter options include: filter by categories, by price and by typing in the search box. I used JavaScript Map and Filter method along with event listeners to manipulate the DOM. Project is styled with SASS and bundled with webpack build tools. Please checkout github for dev dependencies.",
+  // },
 ];
